a7800ed316dcfddb6b499fdeb494944c45df9580
Ganesh Kumar <gk48605@gmail.com>
2025-08-26 05:34:07 +0000

    layout ko fiix karo

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -78,9 +78,9 @@ export default function AdminPanelLayout({
           </SidebarContent>
         </Sidebar>
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-                <SidebarTrigger className="sm:hidden" />
-            </header>
+          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+            <SidebarTrigger className="sm:hidden" />
+          </header>
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
             {children}
           </main>
