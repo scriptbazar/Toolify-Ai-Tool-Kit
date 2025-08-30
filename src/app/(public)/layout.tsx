1a5584015fe00ff8d309dea38a30fee1df9d51bc
Ganesh Kumar <gk48605@gmail.com>
2025-08-30 12:34:35 +0000

    admin pannle me abhi bhi 2 sidebar show ho rahe hai website ke homegpage

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,20 +1,8 @@
 
-import { ChatWidget } from '@/components/common/ChatWidget';
-import Footer from '@/components/common/Footer';
-import Header from '@/components/common/Header';
-import { cn } from '@/lib/utils';
-
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className={cn('relative flex min-h-screen flex-col')}>
-      <Header />
-      <main className="flex-1">{children}</main>
-      <ChatWidget />
-      <Footer />
-    </div>
-  );
+  return <>{children}</>;
 }
