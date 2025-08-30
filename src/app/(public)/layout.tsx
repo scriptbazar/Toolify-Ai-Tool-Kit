b1259b9435cfb697e6b3e73fa19f1cdeaa63bcab
Ganesh Kumar <gk48605@gmail.com>
2025-08-30 13:15:27 +0000

    abhi bhi 2 2 show ho rahe hai

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,8 +1,19 @@
 
+import { ChatWidget } from '@/components/common/ChatWidget';
+import Footer from '@/components/common/Footer';
+import Header from '@/components/common/Header';
+
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <ChatWidget />
+      <Footer />
+    </div>
+  );
 }
