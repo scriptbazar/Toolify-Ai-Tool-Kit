ac6f64a9d1a9a46e4da833174607ef941ba0639f
Ganesh Kumar <gk48605@gmail.com>
2025-09-05 01:52:42 +0000

    poest edit ko feature banao in features ko add karo

---
+++ src/components/common/RichTextEditor.tsx
@@ -0,0 +1,49 @@
+
+'use client';
+
+import React from 'react';
+import 'react-quill/dist/quill.snow.css';
+import dynamic from 'next/dynamic';
+
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+
+interface RichTextEditorProps {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+export function RichTextEditor({ value, onChange, placeholder, className }: RichTextEditorProps) {
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3, false] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link'],
+      [{ 'align': [] }],
+      ['clean']
+    ],
+  };
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike',
+    'list', 'bullet', 'indent',
+    'link',
+    'align',
+  ];
+
+  return (
+    <div className={className}>
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={onChange}
+        modules={modules}
+        formats={formats}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
