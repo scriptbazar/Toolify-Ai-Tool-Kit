048326a5457f6cee9151a59c73cc4fa4089fb1e0
Ganesh Kumar <gk48605@gmail.com>
2025-09-22 17:15:49 +0000

    abhi bhi same error hai abhi bhi tum us error ko fix nahi kar paye ho

--- src/components/tools/tool-components.ts
+++ src/components/tools/tool-components.ts
@@ -293,5 +293,3 @@ export const toolComponents: { [key: string]: React.ComponentType } = {
   'youtube-video-title-extractor': dynamic(() => import('@/components/tools/YouTubeVideoTitleExtractor').then(mod => mod.YouTubeVideoTitleExtractor), { loading: () => <LoadingComponent /> }),
   'youtube-video-downloader': dynamic(() => import('@/components/tools/YoutubeVideoDownloader').then(mod => mod.YoutubeVideoDownloader), { loading: () => <LoadingComponent /> }),
 };
-
-    
