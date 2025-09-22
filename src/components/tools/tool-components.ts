f6b998b8c95f412ec8dfed5e6532317d011156f7
Ganesh Kumar <gk48605@gmail.com>
2025-09-22 16:46:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/tools/tool-components.ts
+++ src/components/tools/tool-components.ts
@@ -1,4 +1,3 @@
-
 import { AdMobRevenueCalculator } from './AdMobRevenueCalculator';
 import { AdSenseRevenueCalculator } from './AdSenseRevenueCalculator';
 import { AddWatermarkToPdf } from './AddWatermarkToPdf';
@@ -280,5 +279,3 @@ export const toolComponents: { [key: string]: React.ComponentType } = {
   'youtube-video-title-extractor': dynamic(() => import('@/components/tools/YouTubeVideoTitleExtractor').then(mod => mod.YouTubeVideoTitleExtractor), { loading: () => <LoadingComponent /> }),
   'youtube-video-downloader': dynamic(() => import('@/components/tools/YoutubeVideoDownloader').then(mod => mod.YoutubeVideoDownloader), { loading: () => <LoadingComponent /> }),
 };
-
-    
