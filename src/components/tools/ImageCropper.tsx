53e70457b6ce4dd2dbe7f74601d4ea68d5c1e5a5
Ganesh Kumar <gk48605@gmail.com>
2025-09-24 12:56:23 +0000

    Try fixing this error: `Runtime Error: Error: Switched to client renderi

--- src/components/tools/ImageCropper.tsx
+++ src/components/tools/ImageCropper.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import * as React from 'react';
 import { useState, useRef } from 'react';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
