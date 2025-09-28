9af954eaf12cf712a254531e26787c4c3cf52101
Ganesh Kumar <gk48605@gmail.com>
2025-09-28 03:36:44 +0000

    abhi bhi add nahi hua interface Text Encryption & Decryption tool ka kya

--- src/components/tools/tool-components.tsx
+++ src/components/tools/tool-components.tsx
@@ -228,7 +228,7 @@ export const toolComponents: { [key: string]: React.ComponentType } = {
   'speech-to-text': SpeechToText,
   'sql-formatter': SqlFormatter,
   'srm-to-cgpa-calculator': SrmToCgpaCalculator,
-  'text-encryption-decryption': TextEncryptionDecryption,
+  'text-encryption-and-decryption': TextEncryptionDecryption,
   'text-repeater': TextRepeater,
   'text-to-binary': TextToBinary,
   'text-to-morse-code': TextToMorseCode,
