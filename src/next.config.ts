136a8b068b2dfbd7246fc9c1020bd8a341f31850
Ganesh Kumar <gk48605@gmail.com>
2025-10-08 01:48:48 +0000

    fix this

---
+++ src/next.config.ts
@@ -0,0 +1,74 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.thum.io',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  webpack: (config) => {
+    //
+    // The "handlebars" library is a sub-dependency of genkit, and it uses
+    // an old "require.extensions" feature that is not supported by webpack.
+    //
+    // This can be worked around by telling webpack to not try and bundle the
+    // "handlebars" library.
+    //
+    config.externals.push('handlebars');
+
+    // This alias is necessary to resolve the 'node:process' import used by
+    // some dependencies, which is not supported by Webpack by default.
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+    if (!config.resolve.alias) {
+      config.resolve.alias = {};
+    }
+    config.resolve.alias['node:process'] = 'process/browser';
+
+    // This is to solve the "topLevelAwait" issue with pdfjs-dist.
+    // It prevents webpack from trying to bundle a node-specific canvas module
+    // on the client.
+    config.externals.push('canvas');
+    
+    return config;
+  },
+};
+
+export default nextConfig;
