9b99958a73dfce11d612ea8d591230b207458689
Ganesh Kumar <gk48605@gmail.com>
2025-10-12 02:47:31 +0000

    mai aapko ek baar fir se keys de raha hau update kar do // Import the fu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,19 +3,24 @@
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyA5Jz8oG_a-x8E8-g9wX3sYvB2eI4U5oHk",
+  apiKey: "AIzaSyCr9mGn2d7OWTX3B-TKxvO3kb7gB_yABdE",
   authDomain: "toolifyai-7sfvi.firebaseapp.com",
   projectId: "toolifyai-7sfvi",
   storageBucket: "toolifyai-7sfvi.appspot.com",
-  messagingSenderId: "1091938363769",
-  appId: "1:1091938363769:web:a37651a8a253965158189c"
+  messagingSenderId: "69818852370",
+  appId: "1:69818852370:web:375b674cfee5e2c863e4cb",
+  measurementId: "G-TPHVVFR0YT"
 };
 
+// Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+// const analytics = getAnalytics(app);
 
 export { app, auth, db };
