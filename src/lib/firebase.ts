e79dd8673bd3a4f4116585aade54ed84f920d285
Ganesh Kumar <gk48605@gmail.com>
2025-10-12 02:48:28 +0000

    abhi bhi error aa raha hai

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyCr9mGn2d7OWTX3B-TKxvO3kb7gB_yABdE",
   authDomain: "toolifyai-7sfvi.firebaseapp.com",
   projectId: "toolifyai-7sfvi",
-  storageBucket: "toolifyai-7sfvi.appspot.com",
+  storageBucket: "toolifyai-7sfvi.firebasestorage.app",
   messagingSenderId: "69818852370",
   appId: "1:69818852370:web:375b674cfee5e2c863e4cb",
   measurementId: "G-TPHVVFR0YT"
