234c9f32f8840798fbe5d149a176a89edc2a0575
Ganesh Kumar <gk48605@gmail.com>
2025-10-18 09:19:07 +0000

    Try fixing this error: `Build Error: Module parse failed: Duplicate expo

--- src/components/tools/index.ts
+++ src/components/tools/index.ts
@@ -151,9 +151,7 @@ export { PpfCalculator } from './PpfCalculator';
 export { SwpCalculator } from './SwpCalculator';
 export { RetirementSavingsCalculator } from './RetirementSavingsCalculator';
 export { MortgageCalculator } from './MortgageCalculator';
-export { AverageCalculator } from './AverageCalculator';
 export { HraCalculator } from './HraCalculator';
 export { TextToHandwritingConverter } from './TextToHandwritingConverter';
 export { TextToAsciiConverter } from './TextToAsciiConverter';
-
     
