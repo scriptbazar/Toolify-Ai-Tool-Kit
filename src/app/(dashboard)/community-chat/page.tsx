02f6ef71ea7ccfa953fe8a50e9a7051861de9702
Ganesh Kumar <gk48605@gmail.com>
2025-10-22 05:21:19 +0000

    kya kya delete kiya mujhe details me batao

--- src/app/(dashboard)/community-chat/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-'use client';
-
-import { CommunityChat } from '@/components/community/CommunityChat';
-
-export default function CommunityChatPage() {
-    return <CommunityChat isAdmin={false} />;
-}
