58ed4590ba2ea7fb9a4110dc5e4e27f44dd2b906
Ganesh Kumar <gk48605@gmail.com>
2026-02-12 19:53:01 +0000

    button not showing

--- src/components/tools/AmazonShippingLabelCropper.tsx
+++ src/components/tools/AmazonShippingLabelCropper.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import * as React from 'react';
@@ -38,7 +37,7 @@ export default function AmazonShippingLabelCropper() {
   return (
     <ShippingLabelCropper
       platform="Amazon"
-      description="Upload the standard 8.5 x 11 inch PDF label you downloaded from Amazon Seller Central."
+      description='Upload the standard 8.5 x 11 inch PDF label you downloaded from Amazon Seller Central.'
       cropFunction={cropAmazonLabel}
     />
   );
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,3 @@
-
 import {
   Type,
   File,
